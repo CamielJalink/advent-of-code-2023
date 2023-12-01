d146c441ca8ebc01d612f19c1afbdaea9e4a45c8
Camiel Jalink <cg.jalink@belastingdienst.nl>
2023-12-01 18:00:38 +0100

    day1part1 solution

---
+++ src/day1/part1/index.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from "fs";
+
+export default function advent() {
+    const stringInput = readFileSync("input/day1.txt", "utf-8");
+    const input = stringInput.split(/\n/gm);
+    input.pop(); // stupid last line added by IntelliJ...
+    console.log(findSum(input));
+}
+
+function findSum(input: string[]){
+    let sum = 0;
+    input.forEach((calibrationString: string) => {
+        sum += findCalibrationValue(calibrationString);
+    })
+    return sum;
+}
+
+function findCalibrationValue(calibrationString: string){
+    const stringArray = calibrationString.match(/[0-9]/g)?.map((value) => value.toString()) as string[];
+    let value = 0;
+
+    if(stringArray.length > 0) {
+        value = parseInt(stringArray[0] + stringArray[stringArray.length - 1]);
+    }
+
+    return value;
+}
