4ea1a291f6bd86229e1d6a87503143b15059731f
Camiel Jalink <cg.jalink@belastingdienst.nl>
2023-12-02 20:44:06 +0100

    day2part1 solution

---
+++ src/day2/part1/index.ts
@@ -0,0 +1,53 @@
+import { readFileSync } from "fs";
+
+export default function advent() {
+    const stringInput = readFileSync("input/day2.txt", "utf-8");
+    const input = stringInput.split(/\n/gm);
+    input.pop();
+    console.log(findSumOfIds(input));
+}
+
+function findSumOfIds(input: string[]){
+    let sumIDs = 0;
+    input.forEach((stringGame: string, id) => {
+        const gameRounds: string[] = getGameRounds(stringGame);
+        let isValid = true;
+        gameRounds.forEach((round: string) => {
+            if(!isValidGame(round)){
+                isValid = false;
+            }
+        });
+
+        if(isValid){
+            sumIDs += id+1; // 0-index index so plus 1
+        }
+    });
+
+    return sumIDs;
+}
+
+function isValidGame(round:string) {
+    let gameIsValid = true;
+    const colors = round.split(", ");
+
+    colors.forEach((color: string) => {
+        const valuePair = color.split(" ");
+        if(valuePair[1] === "red" && parseInt(valuePair[0]) > 12){
+            gameIsValid = false;
+        }
+        else if(valuePair[1] === "green" && parseInt(valuePair[0]) > 13) {
+            gameIsValid = false;
+        }
+        else if(valuePair[1] === "blue" && parseInt(valuePair[0]) > 14) {
+            gameIsValid = false;
+        }
+    });
+
+    return gameIsValid;
+}
+
+function getGameRounds(stringGame: string){
+    const allRounds = stringGame.split(": ")[1];
+    return allRounds.split("; ");
+
+}
