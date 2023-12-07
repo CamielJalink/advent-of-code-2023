5686a97b061cd2c0c04ec4c7547ff99f82f44430
Camiel Jalink <cg.jalink@belastingdienst.nl>
2023-12-07 08:17:40 +0100

    day6part1 solution

--- src/day5/part2/index.ts
+++ src/day5/part2/index.ts
@@ -3,30 +3,9 @@ import { readFileSync } from "fs";
 export default function advent() {
     const stringInput = readFileSync("input/day5-test.txt", "utf-8");
     const input = stringInput.split(/\n\n/gm);
-    console.log(findLowestLocation(input));
+    //console.log(findLowestLocation(input));
 }
 
-function findLowestLocation(input:string[]) {
-    const stringSeeds = input[0];
-    const seeds: Set<bigint> = findSeeds(stringSeeds);
-
-    input.shift();
-    const maps: Map[] = input.map((stringMap) => new Map(stringMap));
-
-    seeds.forEach((seed: bigint) => {
-        maps.forEach((map: Map) => {
-            seed = map.transform(seed);
-        });
-    });
-
-    let lowestLocation = 999999999999999n;
-    seeds.forEach((seed: bigint) => {
-        if(seed < lowestLocation){
-            lowestLocation = seed;
-        }
-    });
-    return lowestLocation;
-}
 
 class Map {
     name = "error";
@@ -53,9 +32,32 @@ class Map {
     }
 }
 
+
 // function findSeeds(stringSeeds: string) {
 //     const stringNumbers = stringSeeds.split(" ");
 //     stringNumbers.shift();
+//
+// function findLowestLocation(input:string[]) {
+//     const stringSeeds = input[0];
+//     const seeds: Set<bigint> = findSeeds(stringSeeds);
+//
+//     input.shift();
+//     const maps: Map[] = input.map((stringMap) => new Map(stringMap));
+//
+//     seeds.forEach((seed: bigint) => {
+//         maps.forEach((map: Map) => {
+//             seed = map.transform(seed);
+//         });
+//     });
+//
+//     let lowestLocation = 999999999999999n;
+//     seeds.forEach((seed: bigint) => {
+//         if(seed < lowestLocation){
+//             lowestLocation = seed;
+//         }
+//     });
+//     return lowestLocation;
+// }
 //
 //     const seeds: Set<bigint> = new Set();
 //     for(let i = 0; i < stringNumbers.length - 1; i+=2){
--- src/day6/part1/index.ts
+++ src/day6/part1/index.ts
@@ -1,7 +1,39 @@
 import { readFileSync } from "fs";
 
 export default function advent() {
-    const stringInput = readFileSync("input/day6-test.txt", "utf-8");
+    const stringInput = readFileSync("input/day6.txt", "utf-8");
     const input = stringInput.split(/\n/gm);
-    console.log(input);
+    console.log(beatRaces(input));
+}
+
+// Time:      7  15   30
+// Distance:  9  40  200
+function beatRaces(input: string[]) {
+    const times = input[0].match(/[0-9]+/g)!.map((stringNumber) => parseInt(stringNumber));
+    const distances = input[1].match(/[0-9]+/g)!.map((stringNumber) => parseInt(stringNumber));
+
+    let solutionScore = 1;
+    for(let i = 0; i < times.length; i++){
+        solutionScore *= raceSolutions(times[i], distances[i]);
+    }
+    return solutionScore;
+}
+
+function raceSolutions(time: number, distance: number){
+    let solutions = 0;
+
+    for(let t = 0; t <= time; t++){
+        if(checkSolution(t, time, distance)){
+            solutions++;
+        }
+    }
+    return solutions;
+}
+
+function checkSolution(pressTime: number, raceTime: number, recordDistance: number){
+    const moveTime = raceTime - pressTime;
+    const distance = moveTime * pressTime;
+    if(distance > recordDistance){
+        return true;
+    }
 }
